f9c5d7059b9d41825546ca0c9d9ec0dce54e8567
adrian61 <ad.kowalskI@gmail.com>
2020-04-28 20:02:52 +0200

    Added structure of files

---
+++ script/script.js
@@ -0,0 +1,5 @@
+function inherit() {
+    document.getElementById("last-btn").width = document.getElementById("first-btn").offsetWidth;
+    document.getElementById("first-btn").width = document.getElementById("last-btn").width;
+
+}
