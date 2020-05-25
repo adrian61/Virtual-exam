302116766f793e83fe454cd187cbe3fae13b6e3a
adrian61 <ad.kowalskI@gmail.com>
2020-05-25 13:19:26 +0200

    Added SpringBoot

---
+++ src/main/java/io/pdsi/virtualexam/VirtualExamApplication.java
@@ -0,0 +1,13 @@
+package io.pdsi.virtualexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualExamApplication.class, args);
+	}
+
+}
