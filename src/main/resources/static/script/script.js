7556d2b939570ec8d16bd809f4fd8df1fffb9d38
adrian61 <ad.kowalskI@gmail.com>
2020-05-25 18:14:37 +0200

    Views adapted to thymeleaf

--- src/main/resources/static/script/script.js
+++
@@ -1,5 +0,0 @@
-function inherit() {
-    document.getElementById("last-btn").width = document.getElementById("first-btn").offsetWidth;
-    document.getElementById("first-btn").width = document.getElementById("last-btn").width;
-
-}
