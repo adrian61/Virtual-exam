99ff9267313bc4280a3c64c1ce3480c3c96e28d0
adrian61 <ad.kowalskI@gmail.com>
2020-05-27 18:26:25 +0200

    Added showing exams of logged user on exam list panel

--- src/main/java/io/pdsi/virtualexam/api/dto/ExamDto.java
+++ src/main/java/io/pdsi/virtualexam/api/dto/ExamDto.java
@@ -4,22 +4,27 @@ import io.pdsi.virtualexam.core.jpa.entity.Exam;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.ZonedDateTime;
+
 @Builder
 @Getter
 public class ExamDto {
 	private Integer id;
 	private String title;
+	private ZonedDateTime startDate;
 
 	public static ExamDto fromEntity(Exam exam) {
 		return ExamDto.builder()
 				.id(exam.getId())
 				.title(exam.getTitle())
+				.startDate(exam.getStartDate())
 				.build();
 	}
 
 	public Exam toEntity() {
 		Exam exam = Exam.builder()
 				.title(this.getTitle())
+				.startDate(this.getStartDate())
 				.build();
 		exam.setId(this.getId());
 		return exam;
