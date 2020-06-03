8a947b9ff60d0005ab78dd403b36671618b48ff5
adrian61 <ad.kowalskI@gmail.com>
2020-06-03 16:53:06 +0200

    small fixes

--- src/main/java/io/pdsi/virtualexam/web/storage/StorageConfiguration.java
+++ src/main/java/io/pdsi/virtualexam/web/storage/StorageConfiguration.java
@@ -2,7 +2,7 @@ package io.pdsi.virtualexam.web.storage;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "storage")
+@ConfigurationProperties(prefix = "file")
 public class StorageConfiguration {
 	private String location;
 
