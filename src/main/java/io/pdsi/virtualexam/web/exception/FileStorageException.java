32d28149d499de8f5f03b6f374daad7add596523
adrian61 <ad.kowalskI@gmail.com>
2020-06-03 16:37:15 +0200

    Added FileStorageException

---
+++ src/main/java/io/pdsi/virtualexam/web/exception/FileStorageException.java
@@ -0,0 +1,11 @@
+package io.pdsi.virtualexam.web.exception;
+
+public class FileStorageException extends RuntimeException {
+	public FileStorageException(String message) {
+		super(message);
+	}
+
+	public FileStorageException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
