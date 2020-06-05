b269adb0cf21698f9461f9dfdc6054e3bca8d310
adrian61 <ad.kowalskI@gmail.com>
2020-06-05 09:28:49 +0200

    Created saveGroups

--- src/main/java/io/pdsi/virtualexam/core/jpa/repository/ExamPathRepository.java
+++ src/main/java/io/pdsi/virtualexam/core/jpa/repository/ExamPathRepository.java
@@ -1,5 +1,9 @@
 package io.pdsi.virtualexam.core.jpa.repository;
 
-public interface ExamPathRepository {
+import io.pdsi.virtualexam.core.jpa.entity.Exam;
+import io.pdsi.virtualexam.core.jpa.entity.ExamPath;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExamPathRepository extends JpaRepository<ExamPath, Integer> {
 
 }
--- src/main/java/io/pdsi/virtualexam/web/service/ExamPathServiceImpl.java
+++ src/main/java/io/pdsi/virtualexam/web/service/ExamPathServiceImpl.java
@@ -1,5 +1,6 @@
 package io.pdsi.virtualexam.web.service;
 
+import io.pdsi.virtualexam.api.dto.ExamPathDto;
 import io.pdsi.virtualexam.core.jpa.repository.ExamPathRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,14 @@ import java.util.ArrayList;
 public class ExamPathServiceImpl {
 	private final ExamPathRepository examPathRepository;
 
-	void saveGroups(Integer examId, ArrayList<String> path){
-
+	void saveGroups(Integer examId, ArrayList<String> pathList) {
+		for (int i = 0; i < pathList.size(); i++) {
+			ExamPathDto examPath = ExamPathDto.builder()
+					.examId(examId)
+					.groupId(i)
+					.path(pathList.get(i))
+					.build();
+			examPathRepository.save(examPath.toEntity());
+		}
 	}
 }
